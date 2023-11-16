ee529d35c8d7243916875d17f09f94ba29cc6cee
Morgan <melarvick@cps.edu>
2023-11-16 11:23:55 -0600

    finished lol

--- a4.py
+++ a4.py
@@ -10,7 +10,47 @@ class TTTBoard:
             represent moves by player 'O' and '*'s are spots no one has yet played on
     """
 
-    pass
+    def __init__(self) -> None:
+        self.board = ['*'] * 9
+
+    def __str__(self) -> str:
+        s = ""
+        for x in [0, 3, 6]:
+            s += self.board[x + 0] + " " + self.board[x + 1] + " " + self.board[x + 2] + "\n"
+        return s
+
+    def make_move(self, player, pos) -> bool:
+        if pos < 0 or pos > 8 or self.board[pos] != '*':
+            return False
+
+        self.board[pos] = player
+        return True        
+
+    def has_won(self, player) -> bool:
+        """Check to see if the player has won"""
+        ps = [player] * 3
+        # Horizontal
+        if self.board[:3] == ps or self.board[3:6] == ps or self.board[6:] == ps:
+            return True
+        # Vertical
+        if self.board[::3] == ps or self.board[1::3] == ps or self.board[2::3] == ps:
+            return True
+        # Diagonal
+        if self.board[::4] == ps or self.board[2:7:2] == ps:
+            return True
+
+        return False
+
+    def game_over(self) -> bool:
+        """Check if the board is full or if either X or O has won"""
+        if "*" not in self.board or self.has_won("X") or self.has_won("O"):
+            return True
+
+        return False
+
+    def clear(self) -> None:
+        """Clear the board to reset game"""
+        self.board = ['*'] * 9
 
 
 def play_tic_tac_toe() -> None:
@@ -61,8 +101,13 @@ if __name__ == "__main__":
     # need to write some more tests to make sure that your TTTBoard class is behaving
     # properly.
     brd = TTTBoard()
+    print(brd.board)
+    print(brd)
     brd.make_move("X", 8)
     brd.make_move("O", 7)
+    brd.make_move("O", 8)
+    brd.make_move("X", 0)
+    print(brd)
 
     assert brd.game_over() == False
 
@@ -70,17 +115,23 @@ if __name__ == "__main__":
     brd.make_move("O", 6)
     brd.make_move("X", 2)
 
+    print(brd)
+
     assert brd.has_won("X") == True
     assert brd.has_won("O") == False
     assert brd.game_over() == True
 
     brd.clear()
 
+    print(brd)
+
     assert brd.game_over() == False
 
     brd.make_move("O", 3)
     brd.make_move("O", 4)
     brd.make_move("O", 5)
+
+    print(brd)
 
     assert brd.has_won("X") == False
     assert brd.has_won("O") == True
@@ -89,4 +140,4 @@ if __name__ == "__main__":
     print("All tests passed!")
 
     # uncomment to play!
-    # play_tic_tac_toe()
+    play_tic_tac_toe()
--- a4_writeup.md
+++ a4_writeup.md
@@ -5,7 +5,10 @@ In assignment 4 we created a basic tic tac toe game so that we could learn objec
 ## Reflection Questions
 
 1. What was the most difficult part to tic-tac-toe?
+Creating the board was the most difficult part of the game for me because it was hard to understand how to list and format everything correctly. 
 
 2. Explain how you would add a computer player to the game.
+You could program a computer to randomly select x or o and place it on the board in competition with the other player and making sure the computer choses a new spot that hasn't been chosen every time. 
 
 3. If you add a computer player, explain (doesn't have to be super technical) how you might get the computer player to play the best move every time. *Note - I am not grading this for a correct answer, I just want to know your thoughts on how you might accomplish it.
+You could somehow have the computer learn from the player's moves/anticipate the patters that they make to block them during the next round. 
